c303b8bb48038cb5de2672cd99bdf026686192f7
boski-src <37199590+boski-src@users.noreply.github.com>
2018-07-26 11:54:17 +0200

    Update payment.php

--- config/payment.php
+++ config/payment.php
@@ -6,9 +6,6 @@ return [
         //'account' => 'yourpaypalemail@domain.com',
         //'link' => 'https://www.paypal.com/cgi-bin/webscr',
         //'ipn' => 'https://ipnpb.paypal.com/cgi-bin/webscr',
-        'account' => 'kubapietras16-facilitator@gmail.com',
-        'link' => 'https://www.sandbox.paypal.com/cgi-bin/webscr',
-        'ipn' => 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr',
     ]
 
 ];
