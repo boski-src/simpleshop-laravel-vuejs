369aab59096b62623738b942a7635a7fec17dc52
boski-src <oskarserafin@evcode.pl>
2019-01-27 18:42:00 +0100

    Update payment.php

--- config/payment.php
+++ config/payment.php
@@ -3,9 +3,9 @@
 return [
 
     'paypal' => [
-        //'account' => 'yourpaypalemail@domain.com',
-        //'link' => 'https://www.paypal.com/cgi-bin/webscr',
-        //'ipn' => 'https://ipnpb.paypal.com/cgi-bin/webscr',
+        'account' => 'yourpaypalemail@domain.com',
+        'link' => 'https://www.paypal.com/cgi-bin/webscr',
+        'ipn' => 'https://ipnpb.paypal.com/cgi-bin/webscr',
     ]
 
 ];
